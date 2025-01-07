4df82046fbae506500ea18faeb71fadcb2f2530d
Shadi Ghantous <54699679+Shadiouss@users.noreply.github.com>
2025-01-07 11:48:53 +0100

    Delete treeCrownDelineation directory

--- treeCrownDelineation/treecrowndelineation/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Max Freudenberg"
-__version__ = "0.1.0"
-
-from . import model
-from . import modules
-from . import dataloading
-
-from .model.tcd_model import TreeCrownDelineationModel
