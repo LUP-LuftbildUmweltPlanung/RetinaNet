80d2a8cbd8c8e0323b972595f4e6342d8606a95d
Shadi Ghantous <54699679+Shadiouss@users.noreply.github.com>
2025-04-01 12:17:27 +0200

    Update mlflow_config.py

--- mlflow_config.py
+++ mlflow_config.py
@@ -2,8 +2,8 @@
 import os
 import mlflow
 
-os.environ["MLFLOW_S3_ENDPOINT_URL"] = "http://192.168.0.75:9000"
+os.environ["MLFLOW_S3_ENDPOINT_URL"] = ""
 os.environ["MLFLOW_S3_IGNORE_TLS"] = "true"
 os.environ["AWS_ACCESS_KEY_ID"] = "your_key"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "your_secret"
-mlflow.set_tracking_uri("http://192.168.0.75:5000")
+mlflow.set_tracking_uri("")
